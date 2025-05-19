3a0cb32b446d55974459dde5fb4d63502e50094b
Erik Rose <erik.w.rose@gmail.com>
2025-05-18 18:52:51 -0700

    feat: Initial dice rolling added with the `Random` struct, containing the `roll_die` and `roll` methods.

--- src/dice.rs
+++ src/dice.rs
@@ -1,3 +1,5 @@
+use rand::distr::Distribution;
+
 #[derive(
     Debug,
     Default,
@@ -15,4 +17,40 @@
 pub struct DieLevel {
     dice: i64,
     pips: i64,
+}
+
+pub struct Random {
+    range: rand::rngs::StdRng,
+    die: rand::distr::Uniform<usize>,
+}
+
+impl Random {
+    pub fn from_seed(seed: u64) -> Result<Self, rand::distr::uniform::Error> {
+        let range = rand::SeedableRng::seed_from_u64(seed);
+        let die = rand::distr::Uniform::new(1, 7)?;
+        Ok(Self { range, die })
+    }
+
+    pub fn roll_die(&mut self) -> usize {
+        self.die.sample(&mut self.range)
+    }
+
+    pub fn roll(&mut self) -> usize {
+        // let mut total = 0;
+        // for _ in 0..3 {
+        //     total += self.die.sample(&mut self.range)
+        // }
+        // total
+        (0..3).fold(0, |acc, _| acc + self.die.sample(&mut self.range))
+    }
+}
+
+impl Default for Random {
+    fn default() -> Self {
+        let mut rng = rand::rng();
+        let range = <rand::rngs::StdRng as rand::SeedableRng>::from_rng(&mut rng);
+        // Quick and dirty method, may panic on ...?
+        let die = rand::distr::Uniform::new(1, 7).unwrap();
+        Self { range, die }
+    }
 }
